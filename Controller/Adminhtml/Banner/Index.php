8b54c024ec8b070dc05f8965fb469b184813af86
EL Harfali <elharfali.abdelkebir@gmail.com>
2019-05-13 21:43:10 +0000

    fix admin grid for banners

--- Controller/Adminhtml/Banner/Index.php
+++ Controller/Adminhtml/Banner/Index.php
@@ -1,10 +1,11 @@
 <?php
 namespace Godogi\BannerSlider\Controller\Adminhtml\Banner;
+
 use Godogi\BannerSlider\Controller\Adminhtml\Banner;
+
 class Index extends Banner
 {
-		public function execute()
-		{
+		public function execute(){
 				$resultPage = $this->_resultPageFactory->create();
 				$resultPage->getConfig()->getTitle()->prepend((__('Banners')));
 				return $resultPage;
